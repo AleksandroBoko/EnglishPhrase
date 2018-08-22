fff52324c6c39478b083b4787bd8ed49e1f34a4b
Oleksandr Bondarenko <ol.bondarenko@andersenlab.com>
2018-08-22 12:26:00 +0300

    Added form to save phrases

--- WebApplication6/Pages/Phrase/Index.cshtml.cs
+++ WebApplication6/Pages/Phrase/Index.cshtml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace WebApplication6.Pages.Phrase
 {
@@ -18,6 +20,18 @@ namespace WebApplication6.Pages.Phrase
         {
             return Page();
         }
+
+        public IActionResult OnGetAddForm()
+        {
+            var partial = new PartialViewResult();
+            partial.ViewName = "_AddingPhrase";
+            partial.ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+            {
+                Model = PhraseInput
+            };
+            
+            return partial;
+    }
 
         public IActionResult OnPost()
         {
---
+++ WebApplication6/wwwroot/js/phrase/phrase.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('#button-add').click(function (event) {        
+        $.ajax({
+            url: '/Phrase/Index?handler=AddForm',
+            success: function (data) {
+                if (data) {
+                    $('#edit-container').html(data);
+                }
+            }
+        });
+    });
+});
