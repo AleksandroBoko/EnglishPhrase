3735955f065af8b2915424edbfd92101c3bcd728
Oleksandr Bondarenko <ol.bondarenko@andersenlab.com>
2018-08-22 12:53:46 +0300

    Added process of saving phrase

--- WebApplication6/Pages/Phrase/Index.cshtml.cs
+++ WebApplication6/Pages/Phrase/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WebApplication6.Models;
 
 namespace WebApplication6.Pages.Phrase
 {
@@ -16,8 +17,21 @@ namespace WebApplication6.Pages.Phrase
         [BindProperty]
         public List<PhraseInput> PhraseInputs { get; set; }
 
+        private readonly ApplicationContext _dbcontext;
+
+        public IndexModel(ApplicationContext applicationContext)
+        {
+            _dbcontext = applicationContext;
+        }
+
         public IActionResult OnGet()
         {
+            var phrases = _dbcontext.Phrases;
+            if (phrases != null && phrases.Any())
+            {
+                PhraseInputs = phrases.Select(x => new PhraseInput() { Id = x.Id, Statement = x.Statement }).ToList();
+            }
+
             return Page();
         }
 
@@ -29,12 +43,21 @@ namespace WebApplication6.Pages.Phrase
             {
                 Model = PhraseInput
             };
-            
+
             return partial;
-    }
+        }
 
         public IActionResult OnPost()
         {
+            var phrase = new Models.Phrase()
+            {
+                Id = 0,
+                Statement = PhraseInput.Statement
+            };
+
+            _dbcontext.Phrases.Add(phrase);
+            _dbcontext.SaveChanges();
+
             return Page();
         }
     }
